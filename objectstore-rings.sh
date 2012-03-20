62c327e9334282b8f360bc29e6bdc00e26eb5b62
Justin Santa Barbara <justin@fathomdb.com>
2012-03-20 14:42:25 -0700

    Split out keystone account creation, better swift install

--- objectstore-rings.sh
+++ objectstore-rings.sh
@@ -1,4 +1,11 @@
+#!/bin/bash
+
+set -e
+
+. utils/common
+
 # TODO: Should this be /var/openstack/objectstore/rings??
+mkdir -p /var/openstack/objectstore/config
 cd /var/openstack/objectstore/config
 
 # We create with REPLICAS=1 so this works on a single-node
@@ -6,36 +13,40 @@ cd /var/openstack/objectstore/config
 # Note that it isn't clear that this patch will be accepted - certainly not in Essex
 REPLICAS=1
 
-swift-ring-builder account.builder create 18 ${REPLICAS} 1
-swift-ring-builder container.builder create 18 ${REPLICAS} 1
-swift-ring-builder object.builder create 18 ${REPLICAS} 1
+export PYTHONPATH=/opt/openstack/swift
+RING_BUILDER=/opt/openstack/swift/bin/swift-ring-builder
+${RING_BUILDER} account.builder create 18 ${REPLICAS} 1
+${RING_BUILDER} container.builder create 18 ${REPLICAS} 1
+${RING_BUILDER} object.builder create 18 ${REPLICAS} 1
 
 # Show rings
-swift-ring-builder account.builder
-swift-ring-builder container.builder
-swift-ring-builder object.builder
+${RING_BUILDER} account.builder
+${RING_BUILDER} container.builder
+${RING_BUILDER} object.builder
 
-export ZONE=1                    # set the zone number for that storage device
-export STORAGE_LOCAL_NET_IP=192.168.90.1    # and the IP address
-export WEIGHT=100               # relative weight (higher for bigger/faster disks)
-export DEVICE=sdb1
-swift-ring-builder account.builder add z$ZONE-$STORAGE_LOCAL_NET_IP:6002/$DEVICE $WEIGHT
-swift-ring-builder container.builder add z$ZONE-$STORAGE_LOCAL_NET_IP:6001/$DEVICE $WEIGHT
-swift-ring-builder object.builder add z$ZONE-$STORAGE_LOCAL_NET_IP:6000/$DEVICE $WEIGHT
+# Add first node to the rings
+export ZONE=1
+export STORAGE_LOCAL_NET_IP=${MY_IP}
+export WEIGHT=100
+export DEVICE=data1
 
-# Show rings
-swift-ring-builder account.builder
-swift-ring-builder container.builder
-swift-ring-builder object.builder
-
-
-swift-ring-builder account.builder rebalance
-swift-ring-builder container.builder rebalance
-swift-ring-builder object.builder rebalance
+${RING_BUILDER} account.builder add z$ZONE-$STORAGE_LOCAL_NET_IP:6002/$DEVICE $WEIGHT
+${RING_BUILDER} container.builder add z$ZONE-$STORAGE_LOCAL_NET_IP:6001/$DEVICE $WEIGHT
+${RING_BUILDER} object.builder add z$ZONE-$STORAGE_LOCAL_NET_IP:6000/$DEVICE $WEIGHT
 
 # Show rings
-swift-ring-builder account.builder
-swift-ring-builder container.builder
-swift-ring-builder object.builder
+${RING_BUILDER} account.builder
+${RING_BUILDER} container.builder
+${RING_BUILDER} object.builder
+
+# "Compile" rings
+${RING_BUILDER} account.builder rebalance
+${RING_BUILDER} container.builder rebalance
+${RING_BUILDER} object.builder rebalance
+
+# Show rings
+${RING_BUILDER} account.builder
+${RING_BUILDER} container.builder
+${RING_BUILDER} object.builder
 
 
